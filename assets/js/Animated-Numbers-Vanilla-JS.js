29e393541c8e4edbcf9ecdeb152905b642bd9e76
june3b <shonibeka@gmail.com>
2023-11-15 11:03:58 +0200

    final home!

---
+++ assets/js/Animated-Numbers-Vanilla-JS.js
@@ -0,0 +1,24 @@
+/*
+ Source : https://codepen.io/akhijannat/pen/JjYQgNK
+*/
+
+const counters = document.querySelectorAll('.counter');
+const speed = 500;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
